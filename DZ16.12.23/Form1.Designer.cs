de7862fa4f4a9715075ca30249d44ff951f9aaca
Lyashenko Sergey <2268746@mail.ru>
2023-12-08 12:36:59 +0300

    end

---
+++ DZ16.12.23/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DZ16._12._23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxGuess = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxGuess
+            // 
+            this.textBoxGuess.Location = new System.Drawing.Point(0, 0);
+            this.textBoxGuess.Name = "textBoxGuess";
+            this.textBoxGuess.Size = new System.Drawing.Size(100, 26);
+            this.textBoxGuess.TabIndex = 0;
+            this.textBoxGuess.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBoxGuess_MouseClick);
+            this.textBoxGuess.Move += new System.EventHandler(this.textBoxGuess_Move);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxGuess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxGuess;
+    }
+}
+
